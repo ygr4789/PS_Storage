582460e07093ab7cf354c76c869356daa3073d22
ygr4789 <yongdori@snu.ac.kr>
2023-01-15 12:26:03 +0900

    ps snippet

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,13 @@
-#include<iostream>
-#include<vector>
-
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    
 }
